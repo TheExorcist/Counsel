37c53a84e00c5f8fba368ad37a7adce39e7f1b9b
Siddartha.jangid <siddartha.jangid@address.com>
2018-04-02 17:30:51 +0530

    add rails, mongo, and react, redux-saga
     Author: Siddartha.jangid@gmail.com

---
+++ client/src/config/configureStore.js
@@ -0,0 +1,8 @@
+import { createStore } from 'redux'
+import * as reducers from './reducers'
+
+const rootReducers = combineReducers(reducers)
+
+const appStore = () => {
+  
+}
---
+++ client/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import Provider from 'react-redux';
+
+
+const RootApp = () => {
+  return
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
